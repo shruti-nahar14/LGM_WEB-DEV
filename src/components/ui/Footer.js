a5a8f9c36b584ebef99f9f3d728119390cb6f656
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-13 13:53:31 +0530

    Add files via upload

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+      <br/>
+       {/* <span>Created By <a link='#'>_____________</a></span>*/}
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <img
+      src={'img/spinner.gif'}
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      alt='Loading'
+    />
+  )
+}
+
+export default Spinner
